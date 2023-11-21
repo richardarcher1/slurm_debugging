793d6b78e73db93866503720b078e8be1d6d89b4
richardarcher1 <richard.archer@yale.edu>
2023-11-21 15:08:31 -0500

    Fixed, solved

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@
 module load anaconda3-2020.07-gcc-9.3.0-myrjwlf
 
 # Creating the conda environment
-conda create -n test_build_2point2 -y
+conda create -n test_build_2point3 -y
 
 # Activating the environment
-conda activate test_build_2point2
+conda activate test_build_2point3
 
 # Setting up the channels
+# have some duplicates to make sure nvidia and pytorch are top of stack
 conda config --env --add channels pytorch
 conda config --env --add channels conda-forge
 conda config --env --add channels defaults
 conda config --env --add channels nvidia
+conda config --env --add channels pytorch
 
 # Installing dependencies one by one
 conda install -y python=3.9 
@@ -22,3 +24,14 @@ conda install -y numpy
 conda install -y scikit-learn 
 conda install -y pytorch=2.1.0 
 conda install -y cudatoolkit=11.8 
+
+# just to check....
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+
+
+# great, that build worked! so, now, grab what I need for the project
+
+conda install -y fire
+conda install -y fairscale
+
+pip install comet_ml
--- minimum_reproducible_example.sh
+++ minimum_reproducible_example.sh
@@ -2,13 +2,3 @@
 
 bash build.sh
 sbatch slurm.sbatch
-
-# that created output1.txt, which failed. So, let's try:
-
-conda install -c nvidia cuda-python
-
-# that created output2.txt, which failed. So, let's try:
-
-conda install pytorch pytorch-cuda=11.8 -c pytorch -c nvidia
-
-# which created failed output3.txt.
